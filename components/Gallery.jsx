4c6890a3adf822deaa0a926ee5d06efbd5059e8d
xDeFc0nx <nehartale8@gmail.com>
2021-12-12 15:15:16 +0100

    fixing bugs adding the image gallery

---
+++ components/Gallery.jsx
@@ -0,0 +1,762 @@
+import React from 'react'
+import G1 from '../images/gallery/G1.jpg'
+import G2 from '../images/gallery/G2.jpg'
+import G3 from '../images/gallery/G3.jpg'
+import G4 from '../images/gallery/G4.jpg'
+import G5 from '../images/gallery/G5.jpg'
+import G6 from '../images/gallery/G6.jpg'
+import G7 from '../images/gallery/G7.jpg'
+import G8 from '../images/gallery/G8.jpg'
+import G9 from '../images/gallery/G9.jpg'
+import G10 from '../images/gallery/G10.jpg'
+import G11 from '../images/gallery/G11.jpg'
+import G12 from '../images/gallery/G12.jpg'
+import G13 from '../images/gallery/G13.jpg'
+import G14 from '../images/gallery/G14.jpg'
+import G15 from '../images/gallery/G15.jpg'
+import G16 from '../images/gallery/G16.jpg'
+import G17 from '../images/gallery/G17.jpg'
+import G18 from '../images/gallery/G18.jpg'
+import G19 from '../images/gallery/G19.jpg'
+import G20 from '../images/gallery/G20.jpg'
+import G21 from '../images/gallery/G21.jpg'
+import G22 from '../images/gallery/G22.jpg'
+import G23 from '../images/gallery/G23.jpg'
+import G24 from '../images/gallery/G24.jpg'
+import G25 from '../images/gallery/G25.jpg'
+import G26 from '../images/gallery/G26.jpg'
+import G27 from '../images/gallery/G27.jpg'
+import G28 from '../images/gallery/G28.jpg'
+import G29 from '../images/gallery/G29.jpg'
+import G30 from '../images/gallery/G30.jpg'
+import G31 from '../images/gallery/G31.jpg'
+import G32 from '../images/gallery/G32.jpg'
+import G33 from '../images/gallery/G33.jpg'
+import G34 from '../images/gallery/G34.jpg'
+import G35 from '../images/gallery/G35.jpg'
+import G36 from '../images/gallery/G36.jpg'
+import G37 from '../images/gallery/G37.jpg'
+import G38 from '../images/gallery/G38.jpg'
+import G39 from '../images/gallery/G39.jpg'
+import G40 from '../images/gallery/G40.jpg'
+import G41 from '../images/gallery/G41.jpg'
+import G42 from '../images/gallery/G42.jpg'
+import G43 from '../images/gallery/G43.jpg'
+import G44 from '../images/gallery/G44.jpg'
+import G45 from '../images/gallery/G45.jpg'
+import G46 from '../images/gallery/G46.jpg'
+import G47 from '../images/gallery/G47.jpg'
+import G48 from '../images/gallery/G48.jpg'
+import G49 from '../images/gallery/G49.jpg'
+import G50 from '../images/gallery/G50.jpg'
+import G51 from '../images/gallery/G51.jpg'
+import G52 from '../images/gallery/G52.jpg'
+import G53 from '../images/gallery/G53.jpg'
+import G54 from '../images/gallery/G54.jpg'
+import G55 from '../images/gallery/G55.jpg'
+import G56 from '../images/gallery/G56.jpg'
+import G57 from '../images/gallery/G57.jpg'
+import G58 from '../images/gallery/G58.jpg'
+import G59 from '../images/gallery/G59.jpg'
+import G60 from '../images/gallery/G60.jpg'
+import G61 from '../images/gallery/G61.jpg'
+import G62 from '../images/gallery/G62.jpg'
+import G63 from '../images/gallery/G63.jpg'
+import G64 from '../images/gallery/G64.jpg'
+import G65 from '../images/gallery/G65.jpg'
+import G66 from '../images/gallery/G66.jpg'
+import G67 from '../images/gallery/G67.jpg'
+import G68 from '../images/gallery/G68.jpg'
+import G69 from '../images/gallery/G69.jpg'
+import G70 from '../images/gallery/G70.jpg'
+import G71 from '../images/gallery/G71.jpg'
+import G72 from '../images/gallery/G72.jpg'
+import G73 from '../images/gallery/G73.jpg'
+import G74 from '../images/gallery/G74.jpg'
+import G75 from '../images/gallery/G75.jpg'
+import G76 from '../images/gallery/G76.jpg'
+import G77 from '../images/gallery/G77.jpg'
+import G78 from '../images/gallery/G78.jpg'
+import G79 from '../images/gallery/G79.jpg'
+import G80 from '../images/gallery/G80.jpg'
+import G81 from '../images/gallery/G81.jpg'
+import G82 from '../images/gallery/G82.jpg'
+import G83 from '../images/gallery/G83.jpg'
+import G84 from '../images/gallery/G84.jpg'
+import G85 from '../images/gallery/G85.jpg'
+import G86 from '../images/gallery/G86.jpg'
+import G87 from '../images/gallery/G87.jpg'
+import G88 from '../images/gallery/G88.jpg'
+import G89 from '../images/gallery/G89.jpg'
+import G90 from '../images/gallery/G90.jpg'
+import G91 from '../images/gallery/G91.jpg'
+import G92 from '../images/gallery/G92.jpg'
+import G93 from '../images/gallery/G93.jpg'
+import G94 from '../images/gallery/G94.jpg'
+import G95 from '../images/gallery/G95.jpg'
+import G96 from '../images/gallery/G96.jpg'
+import G97 from '../images/gallery/G97.jpg'
+import G98 from '../images/gallery/G98.jpg'
+import G99 from '../images/gallery/G99.jpg'
+import G100 from '../images/gallery/G100.jpg'
+import G101 from '../images/gallery/G101.jpg'
+import G102 from '../images/gallery/G102.jpg'
+import G103 from '../images/gallery/G103.jpg'
+import G104 from '../images/gallery/G104.jpg'
+import G105 from '../images/gallery/G105.jpg'
+import G106 from '../images/gallery/G106.jpg'
+import G107 from '../images/gallery/G107.jpg'
+import G108 from '../images/gallery/G108.jpg'
+import G109 from '../images/gallery/G109.jpg'
+import G110 from '../images/gallery/G110.jpg'
+import G111 from '../images/gallery/G111.jpg'
+import G112 from '../images/gallery/G112.jpg'
+import G113 from '../images/gallery/G113.jpg'
+import G114 from '../images/gallery/G114.jpg'
+import G115 from '../images/gallery/G115.jpg'
+import G116 from '../images/gallery/G116.jpg'
+import G117 from '../images/gallery/G117.jpg'
+import G118 from '../images/gallery/G118.jpg'
+import G119 from '../images/gallery/G119.jpg'
+import G120 from '../images/gallery/G120.jpg'
+import G121 from '../images/gallery/G121.jpg'
+import G122 from '../images/gallery/G122.jpg'
+import G123 from '../images/gallery/G123.jpg'
+import G124 from '../images/gallery/G124.jpg'
+import G125 from '../images/gallery/G125.jpg'
+import G126 from '../images/gallery/G126.jpg'
+import G127 from '../images/gallery/G127.jpg'
+import G128 from '../images/gallery/G128.jpg'
+import G129 from '../images/gallery/G129.jpg'
+import G130 from '../images/gallery/G130.jpg'
+import G131 from '../images/gallery/G131.jpg'
+import G132 from '../images/gallery/G132.jpg'
+import G133 from '../images/gallery/G133.jpg'
+import G134 from '../images/gallery/G134.jpg'
+import G135 from '../images/gallery/G135.jpg'
+import G136 from '../images/gallery/G136.jpg'
+import G137 from '../images/gallery/G137.jpg'
+import G138 from '../images/gallery/G138.jpg'
+import G139 from '../images/gallery/G139.jpg'
+import G140 from '../images/gallery/G140.jpg'
+import G141 from '../images/gallery/G141.jpg'
+import G142 from '../images/gallery/G142.jpg'
+import G143 from '../images/gallery/G143.jpg'
+import G144 from '../images/gallery/G144.jpg'
+import G145 from '../images/gallery/G145.jpg'
+import G146 from '../images/gallery/G146.jpg'
+const Gallery = () => {
+
+    let data =[
+     {
+        id: 1,
+        imgSrc:'G1',
+     },
+     {
+        id: 2,
+        imgSrc:'G2',
+     },
+     {
+        id: 3,
+        imgSrc:'G3',
+     },
+     {
+        id: 4,
+        imgSrc:'G4',
+     },
+     {
+        id: 5,
+        imgSrc:'G5',
+     },
+     {
+        id: 6,
+        imgSrc:'G6',
+     },
+     {
+        id: 7,
+        imgSrc:'G7',
+     },
+     {
+        id: 8,
+        imgSrc:'G8',
+     },
+     {
+        id: 9,
+        imgSrc:'G9',
+     },
+     {
+        id: 10,
+        imgSrc:'G10',
+     },
+     {
+        id: 11,
+        imgSrc:'',
+     },
+
+        {
+        id: 12,
+        imgSrc:'',
+     },
+     {
+        id: 13,
+        imgSrc:'',
+     },
+     {
+        id: 14,
+        imgSrc:'',
+     },
+     {
+        id: 15,
+        imgSrc:'',
+     },
+     {
+        id: 16,
+        imgSrc:'',
+     },
+     {
+        id: 17,
+        imgSrc:'',
+     },
+     {
+        id: 18,
+        imgSrc:'',
+     },
+     {
+        id: 19,
+        imgSrc:'',
+     },
+
+     {
+        id: 20,
+        imgSrc:'',
+     },
+     {
+        id: 21,
+        imgSrc:'',
+     },
+     {
+        id: 22,
+        imgSrc:'',
+     },
+     {
+        id: 23,
+        imgSrc:'',
+     },
+     {
+        id: 24,
+        imgSrc:'',
+     },
+     {
+        id: 25,
+        imgSrc:'',
+     },
+     {
+        id: 26,
+        imgSrc:'',
+     },
+
+     {
+        id: 27,
+        imgSrc:'',
+     },
+     {
+        id: 28,
+        imgSrc:'',
+     },
+     {
+        id: 29,
+        imgSrc:'',
+     },
+     {
+        id: 30,
+        imgSrc:'',
+     },
+     {
+        id: 31,
+        imgSrc:'',
+     },
+     {
+        id: 32,
+        imgSrc:'',
+     },
+     {
+        id: 33,
+        imgSrc:'',
+     },
+     {
+        id: 34,
+        imgSrc:'',
+     },
+     {
+        id: 35,
+        imgSrc:'',
+     },
+     {
+        id: 36,
+        imgSrc:'',
+     },
+     {
+        id: 37,
+        imgSrc:'',
+     },
+     {
+        id: 38,
+        imgSrc:'',
+     },
+
+        {
+        id: 39,
+        imgSrc:'',
+     },
+     {
+        id: 40,
+        imgSrc:'',
+     },
+     {
+        id: 41,
+        imgSrc:'',
+     },
+     {
+        id: 42,
+        imgSrc:'',
+     },
+     {
+        id: 43,
+        imgSrc:'',
+     },
+     {
+        id: 44,
+        imgSrc:'',
+     },
+     {
+        id: 45,
+        imgSrc:'',
+     },
+     {
+        id:46,
+        imgSrc:'',
+     },
+
+     {
+        id: 47,
+        imgSrc:'',
+     },
+     {
+        id: 48,
+        imgSrc:'',
+     },
+     {
+        id: 49,
+        imgSrc:'',
+     },
+     {
+        id: 50,
+        imgSrc:'',
+     },
+     {
+        id: 51,
+        imgSrc:'',
+     },
+     {
+        id: 52,
+        imgSrc:'',
+     },
+     {
+        id: 53,
+        imgSrc:'',
+     },
+
+     {
+        id: 54,
+        imgSrc:'',
+     },
+     {
+        id: 55,
+        imgSrc:'',
+     },
+     {
+        id: 56,
+        imgSrc:'',
+     },
+     {
+        id: 57,
+        imgSrc:'',
+     },
+     {
+        id: 58,
+        imgSrc:'',
+     },
+     {
+        id: 59,
+        imgSrc:'',
+     },
+     {
+        id: 60,
+        imgSrc:'',
+     },
+     {
+        id: 61,
+        imgSrc:'',
+     },
+     {
+        id: 62,
+        imgSrc:'',
+     },
+     {
+        id: 63,
+        imgSrc:'',
+     },
+     {
+        id: 64,
+        imgSrc:'',
+     },
+     {
+        id: 65,
+        imgSrc:'',
+     },
+
+        {
+        id: 66,
+        imgSrc:'',
+     },
+     {
+        id: 67,
+        imgSrc:'',
+     },
+     {
+        id: 68,
+        imgSrc:'',
+     },
+     {
+        id: 69,
+        imgSrc:'',
+     },
+     {
+        id: 70,
+        imgSrc:'',
+     },
+     {
+        id: 71,
+        imgSrc:'',
+     },
+     {
+        id: 72,
+        imgSrc:'',
+     },
+     {
+        id: 73,
+        imgSrc:'',
+     },
+
+     {
+        id: 74,
+        imgSrc:'',
+     },
+     {
+        id: 75,
+        imgSrc:'',
+     },
+     {
+        id: 76,
+        imgSrc:'',
+     },
+     {
+        id: 77,
+        imgSrc:'',
+     },
+     {
+        id: 78,
+        imgSrc:'',
+     },
+     {
+        id: 79,
+        imgSrc:'',
+     },
+     {
+        id: 80,
+        imgSrc:'',
+     },
+
+     {
+        id: 81,
+        imgSrc:'',
+     },
+     {
+        id: 82,
+        imgSrc:'',
+     },
+     {
+        id: 83,
+        imgSrc:'',
+     },
+     {
+        id: 84,
+        imgSrc:'',
+     },
+     {
+        id: 85,
+        imgSrc:'',
+     },
+     {
+        id: 86,
+        imgSrc:'',
+     },
+     {
+        id: 87,
+        imgSrc:'',
+     },
+     {
+        id: 88,
+        imgSrc:'',
+     },
+     {
+        id: 89,
+        imgSrc:'',
+     },
+     {
+        id: 90,
+        imgSrc:'',
+     },
+     {
+        id: 91,
+        imgSrc:'',
+     },
+     {
+        id: 92,
+        imgSrc:'',
+     },
+
+        {
+        id: 93,
+        imgSrc:'',
+     },
+     {
+        id: 94,
+        imgSrc:'',
+     },
+     {
+        id: 95,
+        imgSrc:'',
+     },
+     {
+        id: 96,
+        imgSrc:'',
+     },
+     {
+        id: 97,
+        imgSrc:'',
+     },
+     {
+        id: 98,
+        imgSrc:'',
+     },
+     {
+        id: 99,
+        imgSrc:'',
+     },
+     {
+        id: 100,
+        imgSrc:'',
+     },
+
+     {
+        id: 101,
+        imgSrc:'',
+     },
+     {
+        id: 102,
+        imgSrc:'',
+     },
+     {
+        id: 103,
+        imgSrc:'',
+     },
+     {
+        id: 104,
+        imgSrc:'',
+     },
+     {
+        id: 105,
+        imgSrc:'',
+     },
+     {
+        id: 106,
+        imgSrc:'',
+     },
+     {
+        id: 107,
+        imgSrc:'',
+     },
+
+     {
+        id: 108,
+        imgSrc:'',
+     },
+     {
+        id: 109,
+        imgSrc:'',
+     },
+     {
+        id: 110,
+        imgSrc:'',
+     },
+     {
+        id: 112,
+        imgSrc:'',
+     },
+     {
+        id: 113,
+        imgSrc:'',
+     },
+     {
+        id: 114,
+        imgSrc:'',
+     },
+     {
+        id: 115,
+        imgSrc:'',
+     },
+     {
+        id: 116,
+        imgSrc:'',
+     },
+     {
+        id: 117,
+        imgSrc:'',
+     },
+
+        {
+        id: 118,
+        imgSrc:'',
+     },
+     {
+        id: 119,
+        imgSrc:'',
+     },
+     {
+        id: 120,
+        imgSrc:'',
+     },
+     {
+        id: 121,
+        imgSrc:'',
+     },
+     {
+        id: 122,
+        imgSrc:'',
+     },
+     {
+        id: 123,
+        imgSrc:'',
+     },
+     {
+        id: 124,
+        imgSrc:'',
+     },
+     {
+        id: 125,
+        imgSrc:'',
+     },
+
+     {
+        id: 126,
+        imgSrc:'',
+     },
+     {
+        id: 127,
+        imgSrc:'',
+     },
+     {
+        id: 128,
+        imgSrc:'',
+     },
+     {
+        id: 129,
+        imgSrc:'',
+     },
+     {
+        id: 130,
+        imgSrc:'',
+     },
+     {
+        id: 131,
+        imgSrc:'',
+     },
+     {
+        id: 132,
+        imgSrc:'',
+     },
+
+     {
+        id: 133,
+        imgSrc:'',
+     },
+    {
+       id: 134,
+       imgSrc:'',
+    },
+    {
+       id: 135,
+       imgSrc:'',
+    },
+    {
+       id: 136,
+       imgSrc:'',
+    },
+    {
+       id: 137,
+       imgSrc:'',
+    },
+    {
+       id: 138,
+       imgSrc:'',
+    },
+    {
+       id: 138,
+       imgSrc:'',
+    },
+    {
+       id: 139,
+       imgSrc:'',
+    },
+
+    {
+       id: 140,
+       imgSrc:'',
+    },
+    {
+        id: 141,
+        imgSrc:'',
+     },
+     {
+        id: 142,
+        imgSrc:'',
+     },
+     {
+        id: 143,
+        imgSrc:'',
+     },
+     {
+        id: 144,
+        imgSrc:'',
+     },
+     {
+        id: 145,
+        imgSrc:'',
+     },
+     {
+        id: 146,
+        imgSrc:'',
+     },
+     
+    ]
+    
+    return (
+
+        <div>
+            
+        </div>
+    )
+}
+
+export default Gallery
